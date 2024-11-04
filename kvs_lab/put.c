1c893bd7be21297b2f955694312fa5df7ed8a24c
leehyunju02 <163107506+leehyunju02@users.noreply.github.com>
2024-11-04 23:43:49 +0900

    Update put.c

--- kvs_lab/put.c
+++ kvs_lab/put.c
@@ -1,9 +1,46 @@
 #include "kvs.h"
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 int put(kvs_t* kvs, const char* key, const char* value)
 {
-	printf("put: %s, %s\n", key, value);
+    printf("put: %s, %s\n", key, value);
 
-	/* do program here */
-	return 0;
+    node_t* current = kvs->db;
+
+    while (current != NULL) {
+        if (strcmp(current->key, key) == 0) 
+            free(current->value);  // 기존 값 메모리 해제
+            current->value = (char*)malloc(strlen(value) + 1);
+            if (!current->value) {
+                printf("Failed to malloc\n");
+                return -1;
+            }
+            strcpy(current->value, value);
+            return 0;
+        }
+        current = current->next;
+    }
+
+    node_t* new_node = (node_t*)malloc(sizeof(node_t));
+    if (!new_node) {
+        printf("Failed to malloc\n");
+        return -1;
+    }
+    strcpy(new_node->key, key);  
+    new_node->value = (char*)malloc(strlen(value) + 1);
+    if (!new_node->value) {
+        printf("Failed to malloc\n");
+        free(new_node);
+        return -1;
+    }
+    strcpy(new_node->value, value);
+
+    new_node->next = kvs->db;
+    kvs->db = new_node;
+
+    kvs->items++; 
+
+    return 0;
 }
