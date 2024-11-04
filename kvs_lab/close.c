bb101aa3fadaba69260331f8b5cc83730754cf72
leehyunju02 <163107506+leehyunju02@users.noreply.github.com>
2024-11-04 23:40:37 +0900

    Update close.c

--- kvs_lab/close.c
+++ kvs_lab/close.c
@@ -1,7 +1,19 @@
 #include "kvs.h"
+#include <stdlib.h>
 
 int close(kvs_t* kvs)
 {
-	/* do program */
-	return 0;
+    node_t* current = kvs->db;
+    node_t* next;
+
+    while (current != NULL) {
+        next = current->next;  
+        free(current->value);   
+        free(current);          
+        current = next;         
+    }
+
+    free(kvs);
+
+    return 0;
 }
