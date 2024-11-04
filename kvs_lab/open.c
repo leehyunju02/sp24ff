6fa7130b8367db8314dbabd6af1eb3a48a206a6f
leehyunju02 <163107506+leehyunju02@users.noreply.github.com>
2024-11-05 01:32:31 +0900

    Update open.c

--- kvs_lab/open.c
+++ kvs_lab/open.c
@@ -1,13 +1,16 @@
 #include "kvs.h"
-
+#include <stdlib.h>
+#include <stdio.h>
 
 kvs_t* open()
 {
-	kvs_t* kvs = (kvs_t*) malloc (sizeof(kvs_t));
-
-	if(kvs)
-		kvs->items = 0;
-	printf("Open: kvs has %d items\n", kvs->items);
-
-	return kvs;
+    kvs_t* kvs = (kvs_t*)malloc(sizeof(kvs_t));
+    if (kvs) {
+        kvs->items = 0;
+        kvs->db = NULL;  // db를 NULL로 초기화
+        printf("Open: kvs has %d items\n", kvs->items);
+    } else {
+        printf("Failed to allocate memory for kvs\n");
+    }
+    return kvs;
 }
