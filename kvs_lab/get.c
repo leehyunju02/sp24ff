5e8ae80b079ff5fcfc271eba61f83158c4e3cd00
leehyunju02 <163107506+leehyunju02@users.noreply.github.com>
2024-11-04 21:20:27 +0900

    Update get.c

--- kvs_lab/get.c
+++ kvs_lab/get.c
@@ -1,17 +1,26 @@
 #include "kvs.h"
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 char* get(kvs_t* kvs, const char* key)
 {
-	/* do program here */
+    for (int i = 0; i < kvs->size; i++) {
+        if (strcmp(kvs->data[i].key, key) == 0) {
+            strcpy(value, kvs->data[i].value);
+            return value;
+        }
+    }
 
-	char* value = (char*)malloc(sizeof(char)*100);
+    return NULL;
+    
+    char* value = (char*)malloc(sizeof(char)*100);
 
-	if(!value){
-		printf("Failed to malloc\n");
-		return NULL;
-	}
+    if(!value){
+        printf("Failed to malloc\n");
+        return NULL;
+    }
 
-	strcpy(value, "deadbeaf");
-	return value;
-
+    strcpy(value, "deadbeaf");
+    return value;
 }
